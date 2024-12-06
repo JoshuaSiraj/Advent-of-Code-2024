434fd34d07de2aec9e1a16ce4b2eb1e3e1944708
Joshua Siraj <joshua.siraj@ryerson.ca>
2024-12-06 09:00:06 -0500

    day6: cleaned by chatgpt

--- day_6.py
+++ day_6.py
@@ -1,94 +1,78 @@
+
+
 import copy
 from tqdm import tqdm
 
-def part_one(file):
+DIRECTIONS = [(-1, 0), (0, 1), (1, 0), (0, -1)] 
 
-    with open(file, "r") as file:
-        grid = [line.strip() for line in file]
+
+def read_file(file_path):
+    with open(file_path, "r") as file:
+        return [list(line.strip()) for line in file]
+
+
+def move_through_grid(grid, start_pos, allow_loop_check=False):
 
     rows, cols = len(grid), len(grid[0])
-
     visited = set()
-    directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
     curr_direction = 0
 
-    i, j = next((r, row.index('^')) for r, row in enumerate(grid) if '^' in row)
-    visited.add((i, j))
-    
-    
+    i, j = start_pos
+    visited.add((i, j, curr_direction) if allow_loop_check else (i, j))
+
     while True:
-        ni, nj = i, j
-        di, dj = directions[curr_direction]
-        ni, nj = ni + di, nj + dj
-        
-        if (0 > ni or ni >= rows)or (0 > nj or nj >= cols):
-            break
+        ni, nj = i + DIRECTIONS[curr_direction][0], j + DIRECTIONS[curr_direction][1]
+
+        if not (0 <= ni < rows and 0 <= nj < cols):
+            # Out of bounds
+            return visited if not allow_loop_check else False
         elif grid[ni][nj] == '#':
+            # Change direction
             curr_direction = (curr_direction + 1) % 4
+            if allow_loop_check and (i, j, curr_direction) in visited:
+                return True
         else:
-            visited.add((ni, nj))
+            # Move forward
+            if allow_loop_check and (ni, nj, curr_direction) in visited:
+                return True
+            visited.add((ni, nj, curr_direction) if allow_loop_check else (ni, nj))
             i, j = ni, nj
-    
-    return len(visited)
 
-def part_two(file):
+def find_starting_position(grid):
+    for r, row in enumerate(grid):
+        if '^' in row:
+            return r, row.index('^')
 
-    with open(file, "r") as file:
-        grid = [line.strip() for line in file]
-    
-    grid = [list(g) for g in grid]
+def part_one(file_path):
 
-    
-    def check_for_loop(new_grid):
+    grid = read_file(file_path)
+    start_pos = find_starting_position(grid)
+    visited_positions = move_through_grid(grid, start_pos)
+    return len(visited_positions)
 
-        rows, cols = len(new_grid), len(new_grid[0])
-        visited = set()
-        directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-        curr_direction = 0
 
-        start_i, start_j = next((r, row.index('^')) for r, row in enumerate(new_grid) if '^' in row)
-        i, j = start_i, start_j
-        visited.add((i, j, 0))
-        while True:
+def part_two(file_path):
 
-            ni, nj = i, j
-            di, dj = directions[curr_direction]
-            ni, nj = ni + di, nj + dj
-            
-            if (0 > ni or ni >= rows)or (0 > nj or nj >= cols):
-                return False
-            elif new_grid[ni][nj] == '#':
-                curr_direction = (curr_direction + 1) % 4
-                if (i, j, curr_direction) in visited:
-                    return True
-            else:
-                if (ni, nj, curr_direction) in visited:
-                    return True
-                else:
-                    visited.add((ni, nj, curr_direction))
-                    i, j = ni, nj
+    grid = read_file(file_path)
+    start_pos = find_starting_position(grid)
 
-           
-    
-    test_indices = [
-        (row_idx, col_idx)
-        for row_idx, row in enumerate(grid)
-        for col_idx, cell in enumerate(row)
-        if cell == '.'
+    def creates_loop(grid, row, col):
+        grid_copy = copy.deepcopy(grid)
+        grid_copy[row][col] = '#'
+        return move_through_grid(grid_copy, start_pos, allow_loop_check=True)
+
+    empty_cells = [
+        (r, c) for r, row in enumerate(grid) for c, cell in enumerate(row) if cell == '.'
     ]
 
-    res = 0
-    for ci, cj in tqdm(test_indices):
-        new_grid = copy.deepcopy(grid)
-        new_grid[ci][cj] = '#'
+    loop_count = 0
+    for r, c in tqdm(empty_cells, desc="Testing cells for loops"):
+        if creates_loop(grid, r, c):
+            loop_count += 1
 
-        
-        if check_for_loop(new_grid):
-            res += 1
-    
-    return res
-            
-    
-if __name__ == '__main__':
-    print(part_one('inputs/day_6.txt'))
-    print(part_two('inputs/day_6.txt'))
+    return loop_count
+
+
+if __name__ == "__main__":
+    print(f"Part One: {part_one('inputs/day_6.txt')}")
+    print(f"Part Two: {part_two('inputs/day_6.txt')}")
